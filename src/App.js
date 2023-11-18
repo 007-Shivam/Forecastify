2f00462979a072ff2f2c75e52214adf01ebd0e93
007-Shivam <shivambhosle270903@gmail.com>
2023-11-18 15:44:09 +0530

    Added Analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './pages/Home';
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <>
       <Home />
+      <Analytics />
     </>
   );
 }
